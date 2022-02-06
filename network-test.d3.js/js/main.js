f35e95290c4bd7629843514e1438737382218b72
ilsuryusipov <->
2022-02-06 22:25:49 +0300

    -

--- network-test.d3.js/js/main.js
+++ network-test.d3.js/js/main.js
@@ -1,6 +1,8 @@
 var IS_TEXT_LAYERS_HIDDEN = true
 var IS_TIMER_RUN = false;
 var ASYNC_TIMER;
+var MIN_WEIGHT = 1;
+var MAX_WEIGHT = 20;
 
 function randomInteger(min, max) {
 	// получить случайное число от (min-0.5) до (max+0.5)
@@ -17,229 +19,225 @@ function randn_bm() {
 
 //==============================================================================
 
-// let network_data = {
-//   nodes: [
-//     { id: 0, name: 'John Week'},
-//     { id: 1, name: 'John Week'},
-//     { id: 2, name: 'John Week'},
-//     { id: 3, name: 'John Week'},
-//     { id: 4, name: 'John Week'},
-//     { id: 5, name: 'John Week'},
-//     { id: 6, name: 'John Week'},
-//     { id: 7, name: 'John Week'},
-//     { id: 8, name: 'John Week'},
-//     { id: 9, name: 'John Week'},
-//     { id: 10, name: 'John Week'},
-//     { id: 11, name: 'John Week'},
-//     { id: 12, name: 'John Week'},
-//     { id: 13, name: 'John Week'},
-//     { id: 14, name: 'John Week'},
-//     { id: 15, name: 'John Week'},
-//     { id: 16, name: 'John Week'},
-//     { id: 17, name: 'John Week'},
-//     { id: 18, name: 'John Week'},
-//     { id: 19, name: 'John Week'},
-//     { id: 20, name: 'John Week'},
-//     { id: 21, name: 'John Week'},
-//     { id: 22, name: 'John Week'},
-//     { id: 23, name: 'John Week'},
-//     { id: 24, name: 'John Week'},
-//     { id: 25, name: 'John Week'},
-//     { id: 26, name: 'John Week'},
-//     { id: 27, name: 'John Week'},
-//     { id: 28, name: 'John Week'},
-//     { id: 29, name: 'John Week'},
-//     { id: 30, name: 'John Week'},
-//     { id: 31, name: 'John Week'},
-//     { id: 32, name: 'John Week'},
-//     { id: 33, name: 'John Week'},
-//     { id: 34, name: 'John Week'},
-//     { id: 35, name: 'John Week'},
-//     { id: 36, name: 'John Week'},
-//     { id: 37, name: 'John Week'},
-//     { id: 38, name: 'John Week'},
-//     { id: 39, name: 'John Week'},
-//     { id: 40, name: 'John Week'},
-//     { id: 41, name: 'John Week'},
-//     { id: 42, name: 'John Week'},
-//     { id: 43, name: 'John Week'},
-//     { id: 44, name: 'John Week'},
-//     { id: 45, name: 'John Week'},
-//     { id: 46, name: 'John Week'},
-//     { id: 47, name: 'John Week'},
-//     { id: 48, name: 'John Week'},
-//     { id: 49, name: 'John Week'},
-//   ],
-//   links: [
-//     { source: 0, target: 1},
-//     { source: 0, target: 2},
-//     { source: 0, target: 3},
-//     { source: 0, target: 4},
-//     { source: 0, target: 5},
-//     { source: 0, target: 8},
-//     { source: 0, target: 13},
-//     { source: 1, target: 6},
-//     { source: 1, target: 11},
-//     { source: 1, target: 16},
-//     { source: 1, target: 19},
-//     { source: 2, target: 10},
-//     { source: 2, target: 18},
-//     { source: 17, target: 2 },
-//     { source: 17, target: 4 },
-//     { source: 17, target: 7 },
-//     { source: 14, target: 9 },
-//     { source: 17, target: 14 },
-//     { source: 17, target: 12 },
-//     { source: 17, target: 15 },
-//     { source: 17, target: 20 },
-//     { source: 2, target: 21 },
-//     { source: 1, target: 22 },
-//     { source: 2, target: 23 },
-//     { source: 4, target: 24 },
-//     { source: 1, target: 25 },
-//     { source: 2, target: 26 },
-//     { source: 2, target: 28 },
-//     { source: 0, target: 27 },
-//     { source: 1, target: 29 },
-//     { source: 4, target: 30 },
-//     { source: 17, target: 31 },
-//     { source: 4, target: 32 },
-//     { source: 4, target: 33 },
-//     { source: 2, target: 34 },
-//     { source: 4, target: 35 },
-//     { source: 14, target: 36 },
-//     { source: 14, target: 37 },
-//     { source: 14, target: 38 },
-//     { source: 14, target: 39 },
-//     { source: 14, target: 40 },
-//     { source: 14, target: 41 },
-//     { source: 31, target: 42 },
-//     { source: 31, target: 43 },
-//     { source: 31, target: 44 },
-//     { source: 31, target: 45 },
-//     { source: 31, target: 46 },
-//     { source: 31, target: 47 },
-//     { source: 17, target: 48 },
-//     { source: 17, target: 49 },
-//   ]
-// }
+var network_data_1 = {
+  nodes: [
+    { id: 0, name: 'John Week'},
+    { id: 1, name: 'John Week'},
+    { id: 2, name: 'John Week'},
+    { id: 3, name: 'John Week'},
+    { id: 4, name: 'John Week'},
+    { id: 5, name: 'John Week'},
+    { id: 6, name: 'John Week'},
+    { id: 7, name: 'John Week'},
+    { id: 8, name: 'John Week'},
+    { id: 9, name: 'John Week'},
+    { id: 10, name: 'John Week'},
+    { id: 11, name: 'John Week'},
+    { id: 12, name: 'John Week'},
+    { id: 13, name: 'John Week'},
+    { id: 14, name: 'John Week'},
+    { id: 15, name: 'John Week'},
+    { id: 16, name: 'John Week'},
+    { id: 17, name: 'John Week'},
+    { id: 18, name: 'John Week'},
+    { id: 19, name: 'John Week'},
+    { id: 20, name: 'John Week'},
+    { id: 21, name: 'John Week'},
+    { id: 22, name: 'John Week'},
+    { id: 23, name: 'John Week'},
+    { id: 24, name: 'John Week'},
+    { id: 25, name: 'John Week'},
+    { id: 26, name: 'John Week'},
+    { id: 27, name: 'John Week'},
+    { id: 28, name: 'John Week'},
+    { id: 29, name: 'John Week'},
+    { id: 30, name: 'John Week'},
+    { id: 31, name: 'John Week'},
+    { id: 32, name: 'John Week'},
+    { id: 33, name: 'John Week'},
+    { id: 34, name: 'John Week'},
+    { id: 35, name: 'John Week'},
+    { id: 36, name: 'John Week'},
+    { id: 37, name: 'John Week'},
+    { id: 38, name: 'John Week'},
+    { id: 39, name: 'John Week'},
+    { id: 40, name: 'John Week'},
+    { id: 41, name: 'John Week'},
+    { id: 42, name: 'John Week'},
+    { id: 43, name: 'John Week'},
+    { id: 44, name: 'John Week'},
+    { id: 45, name: 'John Week'},
+    { id: 46, name: 'John Week'},
+    { id: 47, name: 'John Week'},
+    { id: 48, name: 'John Week'},
+    { id: 49, name: 'John Week'},
+  ],
+  links: [
+    { id: 0, source: 0, target: 1},
+    { id: 1, source: 0, target: 2},
+    { id: 2, source: 0, target: 3},
+    { id: 3, source: 0, target: 4},
+    { id: 4, source: 0, target: 5},
+    { id: 5, source: 0, target: 8},
+    { id: 6, source: 0, target: 13},
+    { id: 7, source: 1, target: 6},
+    { id: 8, source: 1, target: 11},
+    { id: 9, source: 1, target: 16},
+    { id: 10, source: 1, target: 19},
+    { id: 11, source: 2, target: 10},
+    { id: 12, source: 2, target: 18},
+    { id: 13, source: 17, target: 2 },
+    { id: 14, source: 17, target: 4 },
+    { id: 15, source: 17, target: 7 },
+    { id: 16, source: 14, target: 9 },
+    { id: 17, source: 17, target: 14 },
+    { id: 18, source: 17, target: 12 },
+    { id: 19, source: 17, target: 15 },
+    { id: 20, source: 17, target: 20 },
+    { id: 21, source: 2, target: 21 },
+    { id: 22, source: 1, target: 22 },
+    { id: 23, source: 2, target: 23 },
+    { id: 24, source: 4, target: 24 },
+    { id: 25, source: 1, target: 25 },
+    { id: 26, source: 2, target: 26 },
+    { id: 27, source: 2, target: 28 },
+    { id: 28, source: 0, target: 27 },
+    { id: 29, source: 1, target: 29 },
+    { id: 30, source: 4, target: 30 },
+    { id: 31, source: 17, target: 31 },
+    { id: 32, source: 4, target: 32 },
+    { id: 33, source: 4, target: 33 },
+    { id: 34, source: 2, target: 34 },
+    { id: 35, source: 4, target: 35 },
+    { id: 36, source: 14, target: 36 },
+    { id: 37, source: 14, target: 37 },
+    { id: 38, source: 14, target: 38 },
+    { id: 39, source: 14, target: 39 },
+    { id: 40, source: 14, target: 40 },
+    { id: 41, source: 14, target: 41 },
+    { id: 42, source: 31, target: 42 },
+    { id: 43, source: 31, target: 43 },
+    { id: 44, source: 31, target: 44 },
+    { id: 45, source: 31, target: 45 },
+    { id: 46, source: 31, target: 46 },
+    { id: 47, source: 31, target: 47 },
+    { id: 48, source: 17, target: 48 },
+    { id: 49, source: 17, target: 49 },
+  ]
+}
 
+var network_data_2 = {
+  nodes: [
+    { id: 0, name: 'John Week'},
+    { id: 1, name: 'John Week'},
+    { id: 2, name: 'John Week'},
+    { id: 3, name: 'John Week'},
+    { id: 4, name: 'John Week'},
+    { id: 5, name: 'John Week'},
+    { id: 6, name: 'John Week'},
+    { id: 7, name: 'John Week'},
+    { id: 8, name: 'John Week'},
+    { id: 9, name: 'John Week'},
+    { id: 10, name: 'John Week'},
+    { id: 11, name: 'John Week'},
+    { id: 12, name: 'John Week'},
+    { id: 13, name: 'John Week'},
+    { id: 14, name: 'John Week'},
+    { id: 15, name: 'John Week'},
+    { id: 16, name: 'John Week'},
+    { id: 17, name: 'John Week'},
+    { id: 18, name: 'John Week'},
+    { id: 19, name: 'John Week'},
+    { id: 20, name: 'John Week'},
+    { id: 21, name: 'John Week'},
+    { id: 22, name: 'John Week'},
+    { id: 23, name: 'John Week'},
+    { id: 24, name: 'John Week'},
+    { id: 25, name: 'John Week'},
+    { id: 26, name: 'John Week'},
+    { id: 27, name: 'John Week'},
+    { id: 28, name: 'John Week'},
+    { id: 29, name: 'John Week'},
+    { id: 30, name: 'John Week'},
+    { id: 31, name: 'John Week'},
+    { id: 32, name: 'John Week'},
+    { id: 33, name: 'John Week'},
+    { id: 34, name: 'John Week'},
+    { id: 35, name: 'John Week'},
+    { id: 36, name: 'John Week'},
+    { id: 37, name: 'John Week'},
+    { id: 38, name: 'John Week'},
+    { id: 39, name: 'John Week'},
+    { id: 40, name: 'John Week'},
+    { id: 41, name: 'John Week'},
+    { id: 42, name: 'John Week'},
+    { id: 43, name: 'John Week'},
+    { id: 44, name: 'John Week'},
+    { id: 45, name: 'John Week'},
+    { id: 46, name: 'John Week'},
+    { id: 47, name: 'John Week'},
+    { id: 48, name: 'John Week'},
+    { id: 49, name: 'John Week'},
+  ],
+  links: [
+    { id: 0, source: 0, target: 1},
+    { id: 1, source: 1, target: 2},
+    { id: 2, source: 1, target: 3},
+    { id: 3, source: 1, target: 4},
+    { id: 4, source: 0, target: 5},
+    { id: 5, source: 1, target: 8},
+    { id: 6, source: 0, target: 13},
+    { id: 7, source: 2, target: 6},
+    { id: 8, source: 1, target: 11},
+    { id: 9, source: 6, target: 16},
+    { id: 10, source: 1, target: 19},
+    { id: 11, source: 6, target: 10},
+    { id: 12, source: 6, target: 18},
+    { id: 13, source: 17, target: 2 },
+    { id: 14, source: 6, target: 4 },
+    { id: 15, source: 17, target: 7 },
+    { id: 16, source: 6, target: 9 },
+    { id: 17, source: 17, target: 14 },
+    { id: 18, source: 1, target: 12 },
+    { id: 19, source: 17, target: 15 },
+    { id: 20, source: 17, target: 20 },
+    { id: 21, source: 6, target: 21 },
+    { id: 22, source: 7, target: 22 },
+    { id: 23, source: 7, target: 23 },
+    { id: 24, source: 7, target: 24 },
+    { id: 25, source: 7, target: 25 },
+    { id: 26, source: 2, target: 26 },
+    { id: 27, source: 7, target: 28 },
+    { id: 28, source: 7, target: 27 },
+    { id: 29, source: 7, target: 29 },
+    { id: 30, source: 7, target: 30 },
+    { id: 31, source: 17, target: 31 },
+    { id: 32, source: 14, target: 32 },
+    { id: 33, source: 7, target: 33 },
+    { id: 34, source: 2, target: 34 },
+    { id: 35, source: 7, target: 35 },
+    { id: 36, source: 14, target: 36 },
+    { id: 37, source: 7, target: 37 },
+    { id: 38, source: 14, target: 38 },
+    { id: 39, source: 7, target: 39 },
+    { id: 40, source: 14, target: 40 },
+    { id: 41, source: 14, target: 41 },
+    { id: 42, source: 31, target: 42 },
+    { id: 43, source: 31, target: 43 },
+    { id: 44, source: 31, target: 44 },
+    { id: 45, source: 31, target: 45 },
+    { id: 46, source: 31, target: 46 },
+    { id: 47, source: 31, target: 47 },
+    { id: 48, source: 17, target: 48 },
+    { id: 49, source: 17, target: 49 },
+    { id: 50, source: 24, target: 46 },
+    { id: 51, source: 37, target: 46 },
+    { id: 52, source: 29, target: 46 },
+    { id: 53, source: 23, target: 46 },
+  ]
+}
 
-
-// let network_data = {
-//   nodes: [
-//     { id: 0, name: 'John Week'},
-//     { id: 1, name: 'John Week'},
-//     { id: 2, name: 'John Week'},
-//     { id: 3, name: 'John Week'},
-//     { id: 4, name: 'John Week'},
-//     { id: 5, name: 'John Week'},
-//     { id: 6, name: 'John Week'},
-//     { id: 7, name: 'John Week'},
-//     { id: 8, name: 'John Week'},
-//     { id: 9, name: 'John Week'},
-//     { id: 10, name: 'John Week'},
-//     { id: 11, name: 'John Week'},
-//     { id: 12, name: 'John Week'},
-//     { id: 13, name: 'John Week'},
-//     { id: 14, name: 'John Week'},
-//     { id: 15, name: 'John Week'},
-//     { id: 16, name: 'John Week'},
-//     { id: 17, name: 'John Week'},
-//     { id: 18, name: 'John Week'},
-//     { id: 19, name: 'John Week'},
-//     { id: 20, name: 'John Week'},
-//     { id: 21, name: 'John Week'},
-//     { id: 22, name: 'John Week'},
-//     { id: 23, name: 'John Week'},
-//     { id: 24, name: 'John Week'},
-//     { id: 25, name: 'John Week'},
-//     { id: 26, name: 'John Week'},
-//     { id: 27, name: 'John Week'},
-//     { id: 28, name: 'John Week'},
-//     { id: 29, name: 'John Week'},
-//     { id: 30, name: 'John Week'},
-//     { id: 31, name: 'John Week'},
-//     { id: 32, name: 'John Week'},
-//     { id: 33, name: 'John Week'},
-//     { id: 34, name: 'John Week'},
-//     { id: 35, name: 'John Week'},
-//     { id: 36, name: 'John Week'},
-//     { id: 37, name: 'John Week'},
-//     { id: 38, name: 'John Week'},
-//     { id: 39, name: 'John Week'},
-//     { id: 40, name: 'John Week'},
-//     { id: 41, name: 'John Week'},
-//     { id: 42, name: 'John Week'},
-//     { id: 43, name: 'John Week'},
-//     { id: 44, name: 'John Week'},
-//     { id: 45, name: 'John Week'},
-//     { id: 46, name: 'John Week'},
-//     { id: 47, name: 'John Week'},
-//     { id: 48, name: 'John Week'},
-//     { id: 49, name: 'John Week'},
-//   ],
-//   links: [
-//     { source: 0, target: 1},
-//     { source: 1, target: 2},
-//     { source: 1, target: 3},
-//     { source: 1, target: 4},
-//     { source: 0, target: 5},
-//     { source: 1, target: 8},
-//     { source: 0, target: 13},
-//     { source: 2, target: 6},
-//     { source: 1, target: 11},
-//     { source: 6, target: 16},
-//     { source: 1, target: 19},
-//     { source: 6, target: 10},
-//     { source: 6, target: 18},
-//     { source: 17, target: 2 },
-//     { source: 6, target: 4 },
-//     { source: 17, target: 7 },
-//     { source: 6, target: 9 },
-//     { source: 17, target: 14 },
-//     { source: 1, target: 12 },
-//     { source: 17, target: 15 },
-//     { source: 17, target: 20 },
-//     { source: 6, target: 21 },
-//     { source: 7, target: 22 },
-//     { source: 7, target: 23 },
-//     { source: 7, target: 24 },
-//     { source: 7, target: 25 },
-//     { source: 2, target: 26 },
-//     { source: 7, target: 28 },
-//     { source: 7, target: 27 },
-//     { source: 7, target: 29 },
-//     { source: 7, target: 30 },
-//     { source: 17, target: 31 },
-//     { source: 14, target: 32 },
-//     { source: 7, target: 33 },
-//     { source: 2, target: 34 },
-//     { source: 7, target: 35 },
-//     { source: 14, target: 36 },
-//     { source: 7, target: 37 },
-//     { source: 14, target: 38 },
-//     { source: 7, target: 39 },
-//     { source: 14, target: 40 },
-//     { source: 14, target: 41 },
-//     { source: 31, target: 42 },
-//     { source: 31, target: 43 },
-//     { source: 31, target: 44 },
-//     { source: 31, target: 45 },
-//     { source: 31, target: 46 },
-//     { source: 31, target: 47 },
-//     { source: 17, target: 48 },
-//     { source: 17, target: 49 },
-//     { source: 24, target: 46 },
-//     { source: 37, target: 46 },
-//     { source: 29, target: 46 },
-//     { source: 23, target: 46 },
-//   ]
-// }
-
-
-
-var network_data = {
+var network_data_3 = {
   nodes: [
     { id: 0,  name: 'John Week'},
     { id: 1,  name: 'John Week'},
@@ -356,7 +354,8 @@ var network_data = {
   ]
 }
 
-
+var network_data_graphs = [network_data_1, network_data_2, network_data_3]
+var network_data = None
 
 //==============================================================================
 
@@ -367,7 +366,7 @@ function ForceGraph({
   nodeStrength,
   linkStrength,
   width = 640, // outer width, in pixels
-  height = 100, // outer height, in pixels
+  height = 220, // outer height, in pixels
 
   nodeFill = "black", // node stroke fill (if not using a group color encoding)
   nodeOpacity = 1,
@@ -547,6 +546,7 @@ function addTimer(){
   })
 
 }
+addTimer()
 
 
 function toggle_text_layers(){
@@ -566,7 +566,7 @@ function toggle_text_layers(){
 
 function generate_links_weights(){
   network_data.links.map((item, n) => {
-    item.weight = randomInteger(1, 5);
+    item.weight = randomInteger(MIN_WEIGHT, MAX_WEIGHT);
   })
 }
 
@@ -635,6 +635,7 @@ function mark_random_links(marked_links_count){
 
 
 function apply_settings(){
+  network_data = network_data_graphs[randomInteger(0, 2)]
 
   document.querySelector("#network").innerHTML = "";
 
@@ -645,10 +646,12 @@ function apply_settings(){
   nodeOpacity = document.querySelector("#nodes_opacity_selector").value;
   nodeFillOriginal = document.querySelector("#node_color_selector").value;
   nodeFillMarked = document.querySelector("#marked_node_color_selector").value;
+  nodeRadiusCoefficient = document.querySelector("#nodes_radius_coefficient_selector").value;
   markedLinksCount = document.querySelector("#marked_links_count_selector").value;
   linkStrokeOriginal = document.querySelector("#link_stroke_color_selector").value;
   linkStrokeMarked = document.querySelector("#marked_links_color_selector").value;
-  linkStrokeOpacity = document.querySelector("#link_stroke_opacity_selector").value;
+  // linkStrokeOpacity = document.querySelector("#link_stroke_opacity_selector").value;
+  linkOpacityCoefficient = document.querySelector("#link_stroke_opacity_coefficient_selector").value;
   linkStrokeWidth = document.querySelector("#link_stroke_width_selector").value;
   linkDistance = document.querySelector("#link_distance_selector").value;
   textFill = document.querySelector("#text_color_selector").value;
@@ -670,19 +673,21 @@ function apply_settings(){
     nodeStrokeOpacity: nodeStrokeOpacity, // node stroke opacity
     nodeStroke: nodeStroke,
     nodeOpacity: nodeOpacity,
-    nodeRadius: d => 4*Math.log(10*d.links_count), // node radius, in pixels
+    nodeRadius: d => 4*Math.log(nodeRadiusCoefficient*d.links_count), // node radius, in pixels
+    // nodeRadius: d => 6*Math.sqrt(d.links_count), // node radius, in pixels
     nodeFill: d => d.mark ? nodeFillMarked : nodeFillOriginal, //"currentColor", // node stroke fill (if not using a group color encoding)
     linkStroke: d => d.mark ? linkStrokeMarked : linkStrokeOriginal, // link stroke color
-    linkStrokeOpacity: d => 0.1 + 9*d.weight/100, // link stroke opacity
+    // linkStrokeOpacity: d => 0.1 + 9*d.weight/100, // link stroke opacity
+    linkStrokeOpacity: d => 0.1 + 2*Math.atan(linkOpacityCoefficient * d.weight / MAX_WEIGHT)/Math.PI, // link stroke opacity
     // linkStrokeOpacity: linkStrokeOpacity, // link stroke opacity
-    linkStrokeWidth: d => 3,
+    linkStrokeWidth: linkStrokeWidth,
     // linkStrokeWidth: d => d.weight,
     linkDistance: linkDistance,
     textFill: textFill,
     textFillOpacity: textFillOpacity
   })
 
-  addTimer();
+  // addTimer();
 
   if(IS_TEXT_LAYERS_HIDDEN){
     document.querySelector("#node_text_layer").style.display = "none";
